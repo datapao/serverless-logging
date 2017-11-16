3721e39f42b518781f8ff93d22433348ba3e69c4
gulyasm <mgulyas86@gmail.com>
2017-11-16 11:22:45 +0100

    adds logger

--- README.md
+++ README.md
@@ -12,6 +12,23 @@ To send 1000 log message to the log sytem use the `test.py`.
 python3 test.py <stream-name> 1000
 ```
 
+## Logger
+We provide an out-of-the-box logger with `logger.py`.
+Import the Logger class and use it to log structured messages.
+```python
+# Initiate the logger with the stream/logging name
+logger = KinesisLogger("datapao-logging-3")
+
+# Simple message log
+logger.log({ "level": "INFO", "message": "Failed to run batch job" })
+
+# Batch log messages
+logger.log_batch([
+    { "level": "INFO", "message": "Failed to run batch job" },
+    { "level": "DEBUG", "origin": "testing", "timestamp": 1391203123, "sensor": "temp-0002xb" }
+])
+```
+
 ## Contribution
 Feel free to send any feedback, PR or unneccesary GIFs.
 
---
+++ logger.py
@@ -0,0 +1,41 @@
+import sys
+import boto3
+import json
+
+
+class KinesisLogger():
+    def __init__(self, stream_name):
+        self.stream = stream_name
+        self.firehose = boto3.client('firehose')
+
+    def log(self, message):
+        record = {'Data': bytes(json.dumps(message) + '\n', 'utf-8')}
+        response = self.firehose.put_record(
+            DeliveryStreamName=self.stream, Record=record)
+
+    def log_batch(self, messages):
+        messages_num = len(messages)
+        while messages_num > 0:
+            current_batch_num = min(messages_num, 500)
+            records = [{
+                'Data': bytes(json.dumps(message) + '\n', 'utf-8')
+            } for message in messages]
+            response = self.firehose.put_record_batch(
+                DeliveryStreamName=self.stream, Records=records)
+            if int(response['FailedPutCount']) > 0:
+                print("failed to put {} messages.".format(
+                    response['FailedPutCount']))
+            messages_num -= current_batch_num
+
+
+if __name__ == "__main__":
+    logger = KinesisLogger("datapao-logging-3")
+    logger.log({
+        "name": "mate"
+    })
+    logger.log_batch([{
+        "name": "mate"
+    }, {
+        "origin": "testing",
+        "timestamp": 1391203123
+    }])
